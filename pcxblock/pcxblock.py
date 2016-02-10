f099b0de698e8dc3b53af82ab224c9b02db95927
vesloguzov <vesloguzov@gmail.com>
2016-02-10 12:06:30 +0300

    swap

--- pcxblock/pcxblock.py
+++ pcxblock/pcxblock.py
@@ -258,7 +258,7 @@ class PCXBlock(XBlock):
         )
 
         js_urls = (
-            'static/js/init.js',
+            
             'static/js/js/guid.js',
             'static/js/Utils/Pnt.js',
             'static/js/Utils/caman.full.js',
@@ -277,6 +277,7 @@ class PCXBlock(XBlock):
             'static/js/Crafters/WallCrafter.js',
             'static/js/Utils/jquery-ui.min.js',
             'static/js/Utils/bootstrap.min.js',
+            'static/js/init.js',
             'static/js/pcxblock.js',
         )
 
