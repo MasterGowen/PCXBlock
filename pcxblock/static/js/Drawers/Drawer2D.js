afe392dbfc44584c38d6fa856772ce0c0a8d8ccb
vesloguzov <vesloguzov@gmail.com>
2016-03-02 20:52:34 +0300

    no save in FS

--- pcxblock/static/js/Drawers/Drawer2D.js
+++ pcxblock/static/js/Drawers/Drawer2D.js
@@ -121,7 +121,7 @@ Drawer2D.prototype.Draw = function (objects, currentColor) {
         var dataURL = $this.canvas.toDataURL();
         window.World.SavedResult = dataURL;
         //console.log(dataURL);
-        this.DownloadCanvas(dataURL, 'result.png');
+       // this.DownloadCanvas(dataURL, 'result.png');
         window.World.Crafter.SetResultMode(false);
         $this.canvas.width = $this.canvas.OldWidth;
         $this.canvas.height = $this.canvas.OldHeight;
