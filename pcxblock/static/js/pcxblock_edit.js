61ad09ee120b7145e7b14393ca7e5c1f3f1559bc
vesloguzov <vesloguzov@gmail.com>
2016-03-25 12:55:49 +0300

    data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAB4AAAAQ4CAIAAABnsVYUAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAVP1JREFUeNrs3eF129h6Nl

--- pcxblock/static/js/pcxblock_edit.js
+++ pcxblock/static/js/pcxblock_edit.js
@@ -49,7 +49,7 @@ function PCXBlockEdit(runtime, element, data) {
     };
     $(element).find('input[name=upload_image]')[0].addEventListener("change", readFile, false);
 
-    var tabList = '<li class="action-tabs is-active-tabs" id="main-settings-tab">Основные</li><li class="action-tabs" id="scenario-settings-tab">Сценарий</li><li class="action-tabs" id="advanced-settings-tab">Расширенные</li>';
+    var tabList = '<li class="action-tabs is-active-tabs" id="main-settings-tab">Изображение</li><li class="action-tabs" id="scenario-settings-tab">Основные</li><li class="action-tabs" id="advanced-settings-tab">Расширенные</li>';
 	document.getElementsByClassName("editor-modes action-list action-modes")[0].innerHTML = tabList;
 
 	document.querySelector('#main-settings-tab').onclick = function(){
